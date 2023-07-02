90bf2a651a4441d6d99a73c3275b4e929560e007
David-michelp <davidmichelpereiradesouza60@gmail.com>
2023-07-02 11:42:37 -0300

    v2 heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('equipes','root','',{
-    host: 'localhost',
+const sequelize = new Sequelize('heroku_033a70c3ddc6ebc','baa61e403990b3','d03b231a',{
+    host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     port:3306
 })
